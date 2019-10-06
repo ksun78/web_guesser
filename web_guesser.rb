957aff4107984377fb35bc37df6e4f254fae294a
Kevin Sun <kev.sun78@gmail.com>
2019-10-05 20:35:44 -0700

    now keeps track of games and can reset after winning or running out of guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,26 +1,47 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :number, rand(100)
+@@tries = 5
+
+@@number = rand(100)
 
 get '/' do
 	# erb :index, :locals => {:number => number}
 	guess = params['guess']
 	message, background_color = check_message(guess.to_i)
-	erb :index, :locals => {:number => settings.number, :message => message, :background_color => background_color}
+
+	if @@tries == 0
+		if message != "Correct!"
+			message += "\n -- you ran out of guesses, number reset!"
+		else
+			message += "\n -- you guesses it, number reset!"
+			reset_number()
+		end
+		@@tries = 5
+	end
+	@@tries -= 1
+
+	if message == "Correct!"
+		message += "\n -- you guesses it, number reset!"
+		reset_number()
+		@@tries = 5
+	end
+
+
+	erb :index, :locals => {:number => @@number, :message => message, :background_color => background_color, :guesses_left => @@tries + 1}
 end
 
 def check_message(guess)
-	difference = (guess - settings.number).abs
+	difference = (guess - @@number).abs
 	if difference > 25
-		if guess > settings.number
+		if guess > @@number
 			return "Too high!", "red"
 		end
-		if guess < settings.number
+		if guess < @@number
 			return "Too low!", "red"
 		end
 	elsif difference <= 25 && difference > 0
-		if guess > settings.number
+		if guess > @@number
 			return "Too high", "darksalmon"
 		else
 			return "Too low", "darksalmon"
@@ -28,6 +49,10 @@ def check_message(guess)
 	else
 		return "Correct!", "green"
 	end
+end
+
+def reset_number
+	@@number = rand(100)
 end
 
 
