f21a5ed67308bb7f9ae769d3277e4176aa995331
Kevin Sun <kevsun78@uw.edu>
2019-10-05 18:33:33 -0700

    iteration 3: color-coded responses

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,17 +6,27 @@ set :number, rand(100)
 get '/' do
 	# erb :index, :locals => {:number => number}
 	guess = params['guess']
-	message = check_message(guess.to_i)
-	erb :index, :locals => {:number => settings.number, :message => message}
+	message, background_color = check_message(guess.to_i)
+	erb :index, :locals => {:number => settings.number, :message => message, :background_color => background_color}
 end
 
 def check_message(guess)
-	if guess < settings.number
-		return "Too low!"
-	elsif guess > settings.number
-		return "Too high!"
+	difference = (guess - settings.number).abs
+	if difference > 25
+		if guess > settings.number
+			return "Too high!", "red"
+		end
+		if guess < settings.number
+			return "Too low!", "red"
+		end
+	elsif difference <= 25 && difference > 0
+		if guess > settings.number
+			return "Too high", "darksalmon"
+		else
+			return "Too low", "darksalmon"
+		end
 	else
-		return "Correct!"
+		return "Correct!", "green"
 	end
 end
 
